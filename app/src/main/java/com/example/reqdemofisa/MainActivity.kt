1fe46094d1557fed8935c92f6ab9de098895c0d3
Irving Flores <rfloresg1002@alumno.ipn.mx>
2019-04-16 17:02:58 -0500

    [COMMIT] Second Commit XD

--- app/src/main/java/com/example/reqdemofisa/MainActivity.kt
+++ app/src/main/java/com/example/reqdemofisa/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.reqdemofisa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tableBtn.setOnClickListener{
+            val intent = Intent(this, TableActivity::class.java)
+            //start Table Activity
+            startActivity(intent)
+            Toast.makeText(this, "Table", Toast.LENGTH_SHORT).show()
+        }
+
+        pagerBtn.setOnClickListener {
+            val intent = Intent(this, PagerActivity::class.java)
+            //start PagerActivity
+            startActivity(intent)
+            Toast.makeText(this, "Pager", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/reqdemofisa/util/DemoUtils.kt
@@ -0,0 +1,5 @@
+package com.example.reqdemofisa.util
+
+class DemoUtils {
+
+}
