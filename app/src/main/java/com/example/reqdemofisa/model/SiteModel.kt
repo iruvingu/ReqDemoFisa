8a88d2552a185cc4630e3d9b80c682a6bdb62f64
Irving Flores <rfloresg1002@alumno.ipn.mx>
2019-04-23 12:04:55 -0500

    [COMMIT] Fourth Commit XD

--- app/src/main/java/com/example/reqdemofisa/model/SiteModel.kt
+++ app/src/main/java/com/example/reqdemofisa/model/SiteModel.kt
@@ -1,123 +1,85 @@
 package com.example.reqdemofisa.model
-
 import android.os.Parcel
 import android.os.Parcelable
-import java.io.Serializable
 
-class SiteModel : Parcelable{
- override fun writeToParcel(dest: Parcel?, flags: Int) {
-  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
- }
+data class SiteModel(val name : String = ""
+                     ,val projectionEPRC : Float = 0f
+                     ,val projection_0_14 : Float = 0f
+                     ,val projection_1_29 : Float = 0f
+                     ,val projection30_59 : Float = 0f
+                     ,val projection_60_89 : Float = 0f
+                     ,val projection_90_119 : Float = 0f
+                     ,val projection_120_149 : Float = 0f
+                     ,val projection_150_179 : Float = 0f
+                     ,val projection_90_plus : Float = 0f
+                     ,val projectionTaso : Float = 0f
 
- override fun describeContents(): Int {
-  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
- }
+                     ,val tasoEprc : Float = 0f
+                     ,val taso_0_14 : Float = 0f
+                     ,val taso_30_59 : Float = 0f
+                     ,val taso_90_plus : Float = 0f
 
- //attributes
- var name : String = ""
- var projectionEPRC : Float = 0f
- var projection_0_14 : Float = 0f
- var projection_1_29 : Float = 0f
- var projection30_59 : Float = 0f
- var projection_60_89 : Float = 0f
- var projection_90_119 : Float = 0f
- var projection_120_149 : Float = 0f
- var projection_150_179 : Float = 0f
- var projection_90_plus : Float = 0f
- var projectionTaso : Float = 0f
+                     ,val rankEprc : Int = 0
+                     ,val rank_0_14 : Int = 0
+                     ,val rank_30_59 : Int = 0
+                     ,val rank_90_plus : Int = 0) : Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readFloat(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+    }
 
- var tasoEprc : Float = 0f
- var taso_0_14 : Float = 0f
- var taso_30_59 : Float = 0f
- var taso_90_plus : Float = 0f
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeFloat(projectionEPRC)
+        parcel.writeFloat(projection_0_14)
+        parcel.writeFloat(projection_1_29)
+        parcel.writeFloat(projection30_59)
+        parcel.writeFloat(projection_60_89)
+        parcel.writeFloat(projection_90_119)
+        parcel.writeFloat(projection_120_149)
+        parcel.writeFloat(projection_150_179)
+        parcel.writeFloat(projection_90_plus)
+        parcel.writeFloat(projectionTaso)
+        parcel.writeFloat(tasoEprc)
+        parcel.writeFloat(taso_0_14)
+        parcel.writeFloat(taso_30_59)
+        parcel.writeFloat(taso_90_plus)
+        parcel.writeInt(rankEprc)
+        parcel.writeInt(rank_0_14)
+        parcel.writeInt(rank_30_59)
+        parcel.writeInt(rank_90_plus)
+    }
 
- var rankEprc : Int = 0
- var rank_0_14 : Int = 0
- var rank_30_59 : Int = 0
- var rank_90_plus : Int = 0
+    override fun describeContents(): Int {
+        return 0
+    }
 
- constructor(parcel: Parcel) : this() {
-  name = parcel.readString()
-  projectionEPRC = parcel.readFloat()
-  projection_0_14 = parcel.readFloat()
-  projection_1_29 = parcel.readFloat()
-  projection30_59 = parcel.readFloat()
-  projection_60_89 = parcel.readFloat()
-  projection_90_119 = parcel.readFloat()
-  projection_120_149 = parcel.readFloat()
-  projection_150_179 = parcel.readFloat()
-  projection_90_plus = parcel.readFloat()
-  projectionTaso = parcel.readFloat()
-  tasoEprc = parcel.readFloat()
-  taso_0_14 = parcel.readFloat()
-  taso_30_59 = parcel.readFloat()
-  taso_90_plus = parcel.readFloat()
-  rankEprc = parcel.readInt()
-  rank_0_14 = parcel.readInt()
-  rank_30_59 = parcel.readInt()
-  rank_90_plus = parcel.readInt()
- }
+    companion object CREATOR : Parcelable.Creator<SiteModel> {
+        override fun createFromParcel(parcel: Parcel): SiteModel {
+            return SiteModel(parcel)
+        }
 
- //constructor
- constructor()
-
-
-
- constructor(
-  name: String,
-  projectionEPRC: Float,
-  projection_0_14: Float,
-  projection_1_29: Float,
-  projection30_59: Float,
-  projection_60_89: Float,
-  projection_90_119: Float,
-  projection_120_149: Float,
-  projection_150_179: Float,
-  projection_90_plus: Float,
-  projectionTaso: Float,
-  tasoEprc: Float,
-  taso_0_14: Float,
-  taso_30_59: Float,
-  taso_90_plus: Float,
-  rankEprc: Int,
-  rank_0_14: Int,
-  rank_30_59: Int,
-  rank_90_plus: Int
- ) {
-  this.name = name
-  this.projectionEPRC = projectionEPRC
-  this.projection_0_14 = projection_0_14
-  this.projection_1_29 = projection_1_29
-  this.projection30_59 = projection30_59
-  this.projection_60_89 = projection_60_89
-  this.projection_90_119 = projection_90_119
-  this.projection_120_149 = projection_120_149
-  this.projection_150_179 = projection_150_179
-  this.projection_90_plus = projection_90_plus
-  this.projectionTaso = projectionTaso
-  this.tasoEprc = tasoEprc
-  this.taso_0_14 = taso_0_14
-  this.taso_30_59 = taso_30_59
-  this.taso_90_plus = taso_90_plus
-  this.rankEprc = rankEprc
-  this.rank_0_14 = rank_0_14
-  this.rank_30_59 = rank_30_59
-  this.rank_90_plus = rank_90_plus
- }
-
- constructor(name: String) {
-  this.name = name
- }
-
- companion object CREATOR : Parcelable.Creator<SiteModel> {
-  override fun createFromParcel(parcel: Parcel): SiteModel {
-   return SiteModel(parcel)
-  }
-
-  override fun newArray(size: Int): Array<SiteModel?> {
-   return arrayOfNulls(size)
-  }
- }
-
+        override fun newArray(size: Int): Array<SiteModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
